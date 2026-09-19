a280de34c7babe83615bb605f8a0ffeb7d66f2f9
wednesskor <wednesskor@gmail.com>
2026-09-20 07:12:19 +0900

    Update page.tsx

--- app/old/page.tsx
+++ app/old/page.tsx
@@ -1,11 +1,11 @@
-import VideoScrollHero from "./components/VideoScrollHero";
-import HeroSection from "./components/HeroSection";
-import AboutSection from "./components/AboutSection";
-import WhySection from "./components/WhySection";
-import BenefitsSection from "./components/BenefitsSection";
-import ProcessSection from "./components/ProcessSection";
-import ContactSection from "./components/ContactSection";
-import Navigation from "./components/Navigation";
+import VideoScrollHero from "../components/VideoScrollHero";
+import HeroSection from "../components/HeroSection";
+import AboutSection from "../components/AboutSection";
+import WhySection from "../components/WhySection";
+import BenefitsSection from "../components/BenefitsSection";
+import ProcessSection from "../components/ProcessSection";
+import ContactSection from "../components/ContactSection";
+import Navigation from "../components/Navigation";
 
 export default function Home() {
   return (
