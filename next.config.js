aaf68f3f3117af6d7bc52940b6d861b00aa43ca8
wednesskor <wednesskor@gmail.com>
2026-09-20 07:10:04 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [{ source: '/', destination: '/index.html' }];
+  },
 }
 
 module.exports = nextConfig
-
